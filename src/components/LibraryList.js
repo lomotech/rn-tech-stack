8a1205a988990866ff8b2a6532fc92f694468a6c
Muhammad Syafiq <syafiq@my-sands.com>
2017-07-21 01:19:55 +0800

    Add animation, action, reducer.

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react'
-import {View} from 'react-native'
+import {ListView} from 'react-native'
 import {connect} from 'react-redux'
+import ListItem from './ListItem'
 
-class LibraryList extends Component() {
-  state = {}
+class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    })
+
+    // console.log(this.props)
+    this.dataSource = ds.cloneWithRows(this.props.libraries)
+  }
+
+  renderRow(library) {
+    return <ListItem library={library}/>
+  }
 
   render() {
+    console.log(this.props)
+    // return
     return (
-      <View>LibraryList</View>
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     )
   }
 }
 
 const mapStateToProps = state => {
-  return { libraries: state.libraries }
+  return {libraries: state.libraries}
 }
 
 // connect have 2 part of function
